57f1416e2df46c0cd8d54353c3a95d79904df3aa
Francesco Avitto <thewall1988@hotmail.itgit config --global user.name Francesco>
2021-08-04 21:18:31 +0200

    modifiche dashboard

--- resources/views/revisors/dashboard.blade.php
+++ resources/views/revisors/dashboard.blade.php
@@ -1,88 +1,35 @@
 <x-layout>
-    <style><!-- Slider main container -->
-
    
-        .swiper-container {
-            width: 80%;
-            height: 100px !important;
-        }
-        
-        .trash-wrapper>*{
-            padding-right: 40px
-        }
-        .swiper-slide {
-            text-align: center;
-            font-size: 18px;
-            background: #fff;
-            
-            /* Center slide text vertically */
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: -webkit-flex;
-            display: flex;
-            -webkit-box-pack: center;
-            -ms-flex-pack: center;
-            -webkit-justify-content: center;
-            justify-content: center;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            -webkit-align-items: center;
-            align-items: center;
-        }
-        
-        .swiper-slide img {
-            display: block;
-            /* width: 100%;
-            height: 100%; */
-            object-fit: cover;
-        }
-
-        .swiper-pagination-progressbar .swiper-pagination-progressbar-fill{
-           background-color: #ff9e00; 
-        }
-
-        :root {
-         --swiper-theme-color: #ff9e00;
-          }
-
-
-        #dashboard-view{
-            background-image: url('/img/sign_up_background.jpg');
-            background-size: cover;
-        }
-    </style>
-
-    
     <div class="container-fluid pb-5" id="dashboard-view">
         <div class="container pt-3">
             <div class="row">
                 <div class="col-12">
-                    <h2 class="mt-5 fw-bold text-main text-center">La tua Dashboard</h2>
+                    <h2 class="mt-3 fw-bold text-main text-center">La tua Dashboard</h2>
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col-12">
-                    <h3 class="mt-5  txt-secondary text-center">Pending...</h3>
+                    <h3 class="mt-5 text-main fw-bold text-center">Pending...</h3>
                 </div>
             </div>
-            <div class="row mb-5 " style="height:500px">    
+            <div class="row mb-5 row-slider-pending" style="height:550px">    
                 <!-- Slider main container -->
-                <div class="col-12 d-flex justify-content-center">
-                    <div class="swiper-container mySwiperPending">
-                        <div class="swiper-wrapper">
+                <div class="col-12 d-flex justify-content-center h-slider">
+                    <div class="swiper-container mySwiperPending ">
+                        <div class="swiper-wrapper ">
                             @foreach ($pending as $announcement)
-                            <div class="swiper-slide border">
-                                <div class="card h-100 w-100">
+                            <div class="swiper-slide border border-slide">
+                                <div class="card h-100 w-100 bordo-card">
                                     <img src="
                                         {{
                                         count($announcement->images) ? Storage::url($announcement->images[0]->file) : 'https://via.placeholder.com/250'
                                         }}
                                         " class="h-50 img-fluid" alt="...">
                                     <div class="card-body d-flex flex-column justify-content-around align-items-center">
-                                        <h5 class="card-title">{{$announcement->title}}</h5>
-                                        <p class="card-text">{{$announcement->getPreview()}}</p>
+                                        <h3 class="card-title text-main fw-bold">{{$announcement->title}}</h3>
+                                        <p class="card-text text-main fw-bold">{{$announcement->getPreview()}}</p>
                                         <a href="{{route('revisor.panel', $announcement)}}"
-                                        class="btn btn-outline-main w-50">Modera</a>
+                                        class="btn btn-outline-main w-50 text-main fw-bold">Modera</a>
                                     </div>
                                 </div>
                             </div>
@@ -96,25 +43,25 @@
             </div>
             <div class="row mb-3">
                 <div class="col-12">
-                    <h3 class="mt-5  txt-secondary text-center">Cestino</h3>
+                    <h3 class="mt-5  text-main text-center fw-bold">Cestino</h3>
                 </div>
             </div>
             <div class="row" style="height:300px">
                 <!-- Slider main container -->
-                <div class="swiper-container mySwiperRejected">
+                <div class="swiper-container mySwiperRejected h-slider-bin">
                     <div class="swiper-wrapper trash-wrapper">
                         @foreach ($rejected as $announcement)
                         <div class="swiper-slide mx-1 p-0 ">
                             <div class="swiper-slide">
-                                <div class="card h-100 w-100">
+                                <div class="card h-100 w-100 bordo-card">
                                     <img src="
                                         {{
                                         count($announcement->images) ? Storage::url($announcement->images[0]->file) : 'https://via.placeholder.com/150'
                                         }}
                                         " class="h-50 img-fluid" alt="...">
                                     <div class="card-body d-flex flex-column justify-content-around align-items-center">
-                                        <h5 class="card-title">{{$announcement->title}}</h5>
-                                        <a href="{{route('announcement.show', $announcement)}}" class="btn btn-outline-main w-50">Vedi</a>
+                                        <h5 class="card-title text-main fw-bold">{{$announcement->title}}</h5>
+                                        <a href="{{route('announcement.show', $announcement)}}" class="btn btn-outline-main w-50 text-main fw-bold">Vedi</a>
                                     </div>
                                 </div>
                             </div>
